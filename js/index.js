86efdbd96e1c5f17c6f0cee0faa3bd97594f495a
Johannes Krieg <johannes-daniel.krieg@smail.th-koeln.de>
2025-07-16 01:42:23 +0200

    add dark mode

--- js/index.js
+++ js/index.js
@@ -10,3 +10,21 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     })
 })
+
+const toggleBtn = document.getElementById('theme-toggle');
+const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+// Initial theme
+if (localStorage.getItem('theme')) {
+    document.documentElement.setAttribute('data-theme', localStorage.getItem('theme'));
+} else if (prefersDark) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+}
+
+// Toggle on click
+toggleBtn.addEventListener('click', () => {
+    const current = document.documentElement.getAttribute('data-theme');
+    const newTheme = current === 'dark' ? 'light' : 'dark';
+    document.documentElement.setAttribute('data-theme', newTheme);
+    localStorage.setItem('theme', newTheme);
+});
